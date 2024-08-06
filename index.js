9f37b3e05de3fe0b17b15f5c8d9c89f43a4ba5be
Dan Leach <web@danleach.uk>
2024-08-06 19:37:21 +0100

    Moved imports to route level where possible

--- index.js
+++ index.js
@@ -9,19 +9,13 @@ const {
   calculateRules,
 } = require("./validateAndSanitize");
 const keys = require("./private/keys.json");
-const { calculateVariables } = require("./calculateVariables");
-const { generateAuditID } = require("./generateAuditID");
-const { insertCalculateData, updateData } = require("./insertData");
-const { getImdDecile } = require("./getImdDecile");
-const { updateCheck } = require("./updateCheck");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 //required to get the client IP address as server behind proxy
-app.set('trust proxy', 1);
-
+app.set("trust proxy", 1);
 
 /**
  * Rehashes the patient hash with salt.
@@ -54,30 +48,43 @@ app.get("/", (req, res) => {
  */
 app.post("/calculate", calculateRules, validateRequest, async (req, res) => {
   try {
+    const { calculateVariables } = require("./calculateVariables");
+    const { generateAuditID } = require("./generateAuditID");
+    const { insertCalculateData } = require("./insertData");
+    const { getImdDecile } = require("./getImdDecile");
+
+    //get the validated data
     const data = matchedData(req);
 
+    //get the IP address of the client request
     const clientIP = req.ip;
 
+    //perform the calculations and check for errors
     const calculations = calculateVariables(data);
     if (calculations.errors.length) {
       res.status(400).json({ errors: calculations.errors });
       return;
     }
 
+    //set undefined optional values to null
     data.bicarbonate = data.bicarbonate || null;
     data.glucose = data.glucose || null;
     data.ketones = data.ketones || null;
 
+    //perform the 2nd stage hashing with salt
     const patientHash = data.patientHash
       ? rehashPatientHash(data.patientHash)
       : null;
 
+    //get the imdDecile from the postcode
     const imdDecile = data.patientPostcode
       ? await getImdDecile(data.patientPostcode)
       : null;
 
+    //generate a new unique auditID
     const auditID = await generateAuditID();
 
+    //insert the data into the database
     await insertCalculateData(
       data,
       imdDecile,
@@ -87,6 +94,7 @@ app.post("/calculate", calculateRules, validateRequest, async (req, res) => {
       calculations
     );
 
+    //respond to the client with the auditID and the calculations
     res.json({
       auditID,
       calculations,
@@ -106,35 +114,34 @@ app.post("/calculate", calculateRules, validateRequest, async (req, res) => {
  */
 app.post("/update", updateRules, validateRequest, async (req, res) => {
   try {
-    //get the submitted data that passed validation  
+    const { updateData } = require("./insertData");
+    const { updateCheck } = require("./updateCheck");
+
+    //get the submitted data that passed validation
     const data = matchedData(req);
-    
+
     //get the patientHash in the database for given audit ID to check correct patient
     const check = await updateCheck(data.auditID);
-    
+
     //check the updateCheck found a record
     if (!check) {
-        res
-        .status(404)
-        .json(
-          `Audit ID [${data.auditID}] not found in database`
-        );
-      return
+      res.status(404).json(`Audit ID [${data.auditID}] not found in database`);
+      return;
     }
 
     //check the episode has a patientHash
     if (!check.patientHash) {
       res
-      .status(406)
-      .json(
-        `The episode matching the audit ID [${data.auditID}] was created without providing an NHS number. Retrospective audit data updates are therefore not accepted.`
-      );
-    return
-  }
-    
+        .status(406)
+        .json(
+          `The episode matching the audit ID [${data.auditID}] was created without providing an NHS number. Retrospective audit data updates are therefore not accepted.`
+        );
+      return;
+    }
+
     //perform the second step hash before checking patientHash matches
     const patientHash = rehashPatientHash(data.patientHash);
-    
+
     //check the submitted patientHash matches the database patient hash
     if (check.patientHash != patientHash) {
       res
@@ -142,9 +149,9 @@ app.post("/update", updateRules, validateRequest, async (req, res) => {
         .json(
           `Patient NHS number or date of birth do not match for episode with audit ID: ${data.auditID}`
         );
-      return
+      return;
     }
-    
+
     //update the database with new data
     await updateData(data);
 
